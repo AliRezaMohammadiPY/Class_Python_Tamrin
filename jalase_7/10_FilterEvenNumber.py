d58f94b663fe6d47e1699dbafef439c53ec975e8
AliRezaMohammadiPY <13alirezamohammadi88@gmail.com>
2024-10-24 17:12:00 +0330

    Tamrin Filter Even Number Updated

--- jalase_7/10_FilterEvenNumber.py
+++ jalase_7/10_FilterEvenNumber.py
@@ -1,17 +1,22 @@
 def EvenNumber(number):
-        if number % 2 == 0:
-            return number
+    if number % 2 == 0:
+        return number
+
+
 if __name__ == '__main__':
     list_numbers = []
-    while True:
-        try:
-            num = input('yek adad vared konid : ')
-            if num == "end":
-                break
-            else:
-                num = int(num)
-                list_numbers.append(num)
-        except ValueError:
-            print("adad sahih vared konid!")
-    filter_Numbers = filter(EvenNumber, list_numbers)
+
+    num = input('list adad ra vared konid : ')
+    num = num.split(",")
+    for item in num:
+        item = item.replace("[", "").replace("]", "").replace(" ", "")
+        list_numbers.append(item)
+    try:
+        int_list = map(int, list_numbers)
+    except ValueError:
+        print("*" * 40)
+        print("Error!\nAdad sahih vared konid!")
+        print("*" * 40)
+
+    filter_Numbers = filter(EvenNumber, int_list)
     print(f"List adaad zoj = {list(filter_Numbers)}")
