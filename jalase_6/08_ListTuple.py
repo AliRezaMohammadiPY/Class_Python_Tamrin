30b546a78cbd0cd02c48dca2978b1bf7acfb2ec3
AliRezaMohammadiPY <13alirezamohammadi88@gmail.com>
2024-10-24 17:11:01 +0330

    Tamrin List Tuple Updated

--- jalase_6/08_ListTuple.py
+++ jalase_6/08_ListTuple.py
@@ -14,7 +14,7 @@ while True:
     break
 for ch in range(tedad_tuple):
     tuples = input("Tuple Ra vared konid : ")
-    tuple_list = lambda tup:tuples.split()
+    tuple_list = lambda tup: tuples.split()
     tuples = tuple_list(tuples)
     if len(tuples) < 2:
         print("Error! Tuple az 2 ozv kamtar ast. (Please Try Again)")
@@ -23,7 +23,7 @@ for ch in range(tedad_tuple):
         print("Error! Tuple az 2 ozv bishtar ast. (Please Try Again)")
         break
     else:
-        tuple_kon = lambda tuplekon:tuple(tuples)
+        tuple_kon = lambda tuplekon: tuple(tuples)
         tuples = tuple_kon(tuples)
         list_tuples.append(tuples[1])
 print(list_tuples)
